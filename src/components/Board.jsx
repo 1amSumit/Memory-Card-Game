666706a7771e093b5682e8ecc46b6c6b25296190
1amSumit <sj779619@gmail.com>
2024-11-24 14:11:47 +0530

    game board done

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -6,12 +6,13 @@ import { useEffect, useState } from "react";
 
 export default function Board() {
   const cards = [
-    { id: 1, name: "plant1", icon: <RiPlantLine className="w-4 h-4" /> },
-    { id: 2, name: "plant2", icon: <TbPlant2 className="w-4 h-4" /> },
-    { id: 3, name: "plant3", icon: <PiPlant className="w-4 h-4" /> },
+    { id: 1, name: "plant 1", icon: <RiPlantLine className="w-8 h-8" /> },
+    { id: 2, name: "plant 2", icon: <TbPlant2 className="w-8 h-8" /> },
+    { id: 3, name: "plant 3", icon: <PiPlant className="w-8 h-8" /> },
   ];
   const [shuffledCards, setShuffledCards] = useState([]);
   const [flippedCard, setFlippedCard] = useState([]);
+  const [matchedPair, setMatchedPair] = useState(0);
 
   useEffect(() => {
     const dupliacteCards = [...cards, ...cards];
@@ -19,6 +20,7 @@ export default function Board() {
       .sort(() => Math.random() - 0.5)
       .map((card, index) => ({
         id: index,
+        name: card.name,
         icon: card.icon,
         isFlipped: false,
         isMatched: false,
@@ -27,10 +29,32 @@ export default function Board() {
     setShuffledCards(shuffleCards);
   }, []);
 
+  useEffect(() => {
+    if (flippedCard.length === 2) {
+      const [firstCard, secondCard] = flippedCard;
+      if (shuffledCards[firstCard].name === shuffledCards[secondCard].name) {
+        setShuffledCards((prevCards) =>
+          prevCards.map((card, index) =>
+            index === firstCard || index === secondCard
+              ? { ...card, isMatched: true }
+              : card
+          )
+        );
+        setMatchedPair((prev) => prev + 1);
+      }
+
+      setTimeout(() => setFlippedCard([]), 1000);
+    }
+  }, [flippedCard]);
+
   const handleCardClick = (id) => {
-    console.log(id);
     if (flippedCard.length === 2) return;
     setFlippedCard((prev) => [...prev, id]);
+    setShuffledCards((prevCards) =>
+      prevCards.map((card, i) =>
+        i === id ? { ...card, isFlipped: true } : card
+      )
+    );
   };
 
   console.log(flippedCard);
@@ -40,16 +64,25 @@ export default function Board() {
       <div className="grid grid-cols-3 w-full max-w-md items-center justify-center gap-2">
         {shuffledCards.map((card, index) => (
           <motion.div
-            animate={{ rotateY: flippedCard.includes(index) ? 180 : 0 }}
+            animate={{
+              rotateY:
+                flippedCard.includes(card.id) || card.isMatched ? 180 : 0,
+            }}
             transition={{ duration: 0.4 }}
             key={index}
             onClick={() => handleCardClick(card.id)}
             className={`w-[5rem] h-[5rem] rounded-xl  cursor-pointer flex items-center justify-center ${
-              flippedCard.includes(index) ? "bg-purple-600" : "bg-yellow-400"
+              flippedCard.includes(card.id) || card.isMatched
+                ? "bg-purple-600"
+                : "bg-yellow-400"
             }`}
           >
             <div
-              className={`${flippedCard.includes(index) ? "block" : "hidden"}`}
+              className={`${
+                flippedCard.includes(card.id) || card.isMatched
+                  ? "block"
+                  : "hidden"
+              }`}
             >
               {card.icon}
             </div>

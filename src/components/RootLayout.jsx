39f35f39b7aebfc848cb0c86bb96c26fd3085143
1amSumit <sj779619@gmail.com>
2024-12-02 23:11:32 +0530

    board modified

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,13 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { usernameState } from "../store/username";
-import {
-  MdOutlineMusicNote as MusicOn,
-  MdOutlineMusicOff as MusicOff,
-} from "react-icons/md";
 
-import { CiPause1 as Pause } from "react-icons/ci";
 import {
   easyHighScoreState,
   hardHighScoreState,
@@ -20,9 +15,6 @@ import {
 export default function RootLayout() {
   const [usr, setUsr] = useRecoilState(usernameState);
   const navigate = useNavigate();
-  const audioRef = useRef(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [pauseHover, setPauseHover] = useState(false);
 
   const [scoreEasy, setScoreEasy] = useRecoilState(userScoreEasyState);
   const [scoreMedium, setScoreMedium] = useRecoilState(userScoreMediumState);
@@ -53,16 +45,6 @@ export default function RootLayout() {
     setHardHighScore(hardHighScore);
   }, []);
 
-  const toggleMusic = () => {
-    if (isPlaying) {
-      audioRef.current.pause();
-      setIsPlaying(false);
-    } else {
-      audioRef.current.play();
-      setIsPlaying(true);
-    }
-  };
-
   useEffect(() => {
     const username = localStorage.getItem("username") || "";
 
@@ -74,59 +56,11 @@ export default function RootLayout() {
     }
   }, [navigate, setUsr]);
 
-  useEffect(() => {
-    const handleKeyPress = (event) => {
-      if (event.key === "Escape") {
-        navigate("/play");
-      } else if (event.key === "m" || event.key === "M") {
-        toggleMusic();
-      }
-    };
-
-    document.addEventListener("keydown", handleKeyPress);
-
-    return () => {
-      document.removeEventListener("keydown", handleKeyPress);
-    };
-  }, [navigate, toggleMusic]);
-
   return (
     <main className="h-[100vh] bg-gray-900 p-1 lg:p-4 overflow-hidden flex flex-col">
       <div className="h-[90vh]">
         <Outlet />
       </div>
-      <footer className="h-[10vh] flex flex-row gap-4">
-        <div className="">
-          {isPlaying ? (
-            <MusicOn
-              onClick={toggleMusic}
-              className="text-gray-200  w-6 h-6 cursor-pointer"
-            />
-          ) : (
-            <MusicOff
-              onClick={toggleMusic}
-              className="text-gray-200  w-6 h-6 cursor-pointer"
-            />
-          )}
-        </div>
-        <div>
-          <Pause
-            onClick={() => navigate("/play")}
-            onMouseLeave={() => {
-              setPauseHover(false);
-            }}
-            onMouseEnter={() => {
-              setPauseHover(true);
-            }}
-            className="text-gray-200 w-6 h-6 cursor-pointer"
-          />
-          {pauseHover && (
-            <span className="text-white font-sour">Pause game</span>
-          )}
-        </div>
-      </footer>
-
-      <audio ref={audioRef} src="/gameMusic.mp3" loop autoPlay />
     </main>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,28 +13,67 @@ import { gameLevelState } from "../store/level";
 import { matchLevelState } from "../store/match";
 import { turnState } from "../store/moves";
 import BoardMedium from "../components/BoardMedium";
-import BoardHigh from "../components/BoardMedium";
 import BoardHard from "../components/BoardHard";
 import BoardExtreme from "../components/BoardExtreme";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { playingState } from "../store/music";
+import {
+  MdOutlineMusicNote as MusicOn,
+  MdOutlineMusicOff as MusicOff,
+} from "react-icons/md";
+
+import { CiPause1 as Pause } from "react-icons/ci";
 
 export default function Home() {
+  const [isPlaying, setIsPlaying] = useRecoilState(playingState);
   const username = useRecoilValue(usernameState);
   const highScoreUser = useRecoilValue(highScoreUsername);
-  const [scoreEasy, setScoreEasy] = useRecoilState(userScoreEasyState);
-  const [scoreMedium, setScoreMedium] = useRecoilState(userScoreMediumState);
-  const [scoreHard, setScoreHard] = useRecoilState(userScoreHardState);
+  const [scoreEasy] = useRecoilState(userScoreEasyState);
+  const [scoreMedium] = useRecoilState(userScoreMediumState);
+  const [scoreHard] = useRecoilState(userScoreHardState);
 
-  const [easyHighScore, setEasyHighScore] = useRecoilState(easyHighScoreState);
-  const [mediumHighScore, setMediumScore] =
-    useRecoilState(mediumHighScoreState);
-  const [hardHighScore, setHardHighScore] = useRecoilState(hardHighScoreState);
+  const [easyHighScore] = useRecoilState(easyHighScoreState);
+  const [mediumHighScore] = useRecoilState(mediumHighScoreState);
+  const [hardHighScore] = useRecoilState(hardHighScoreState);
 
   const level = useRecoilValue(gameLevelState);
   const match = useRecoilValue(matchLevelState);
-  const [turns, setTurns] = useRecoilState(turnState);
+  const [turns] = useRecoilState(turnState);
+
+  const navigate = useNavigate();
+  const audioRef = useRef(null);
+  const [pauseHover, setPauseHover] = useState(false);
+
+  const toggleMusic = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(false);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(true);
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key === "Escape") {
+        navigate("/play");
+      } else if (event.key === "m" || event.key === "M") {
+        console.log("hakkk");
+        toggleMusic();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [navigate, toggleMusic]);
 
   return (
-    <div>
+    <div className="flex flex-col justify-between h-screen">
       <div className="flex flex-row justify-between">
         <div className="flex flex-col gap-[1rem]">
           <h2 className="text-gray-200 font-bold font-sour text-xl 2xl:text-4xl">
@@ -86,6 +125,38 @@ export default function Home() {
           <BoardExtreme />
         )}
       </div>
+      <footer className="h-[10vh] flex flex-row gap-4">
+        <div className="">
+          {isPlaying ? (
+            <MusicOn
+              onClick={toggleMusic}
+              className="text-gray-200  w-6 h-6 cursor-pointer"
+            />
+          ) : (
+            <MusicOff
+              onClick={toggleMusic}
+              className="text-gray-200  w-6 h-6 cursor-pointer"
+            />
+          )}
+        </div>
+        <div>
+          <Pause
+            onClick={() => navigate("/play")}
+            onMouseLeave={() => {
+              setPauseHover(false);
+            }}
+            onMouseEnter={() => {
+              setPauseHover(true);
+            }}
+            className="text-gray-200 w-6 h-6 cursor-pointer"
+          />
+          {pauseHover && (
+            <span className="text-white font-sour">Pause game</span>
+          )}
+        </div>
+      </footer>
+
+      <audio ref={audioRef} src="/gameMusic.mp3" loop />
     </div>
   );
 }
